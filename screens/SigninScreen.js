d443ba16064fd55c08d4f470731e0553fea46312
Chaehoon Lim <sizplay@gmail.com>
2019-10-25 20:07:14 +0900

    add my work

---
+++ screens/SigninScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import styled from 'styled-components';
+import {Auth} from 'aws-amplify';
+import {Button} from 'native-base';
+
+const SigninScreen = props => {
+  const handleSignin = async () => {
+    try {
+      const signin = await Auth.signIn('abc@gmail.com', '1234qwer');
+      console.log(signin);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Container>
+      <Title>Signin page</Title>
+      <SigninButton onPress={() => handleSignin()}>
+        <SigninText>Sign in</SigninText>
+      </SigninButton>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 32px;
+  color: gray;
+`;
+
+const SigninButton = styled(Button)`
+  margin-top: 10px;
+`;
+
+const SigninText = styled.Text`
+  width: 100px;
+  font-size: 16px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+SigninScreen.navigationOptions = {
+  title: 'Sign In or Register',
+};
+
+export default SigninScreen;
